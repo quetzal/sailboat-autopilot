a69a0d555bfe537c40d1b8801ae60e2705b0340f
PépéMax <xdpepemaxdx@gmail.com>
2018-01-12 18:07:18 +0100

    Display NMEA Cap on OLED SSD1306 display

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <WiFi.h>
 #include "constantes.h"
+#include "SSD1306.h" //https://github.com/squix78/esp8266-oled-ssd1306
+#include <Wire.h>
+
+SSD1306  display(0x3c, 5, 4);
 
 #define MAX_NMEA_LENGHT 90
 
@@ -14,18 +18,25 @@ String NMEA_Header;
 
 String getValue(String data, char separator, int index)
 {
-    int found = 0;
-    int strIndex[] = { 0, -1 };
-    int maxIndex = data.length() - 1;
+  int found = 0;
+  int strIndex[] = { 0, -1 };
+  int maxIndex = data.length() - 1;
 
-    for (int i = 0; i <= maxIndex && found <= index; i++) {
-        if (data.charAt(i) == separator || i == maxIndex) {
-            found++;
-            strIndex[0] = strIndex[1] + 1;
-            strIndex[1] = (i == maxIndex) ? i+1 : i;
-        }
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i+1 : i;
     }
-    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void display_nmea(String s){
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+  display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, s);
+  display.display();
 }
 
 void setup() {
@@ -35,28 +46,38 @@ void setup() {
   tmpNmeaSentence = (char*)malloc(MAX_NMEA_LENGHT*sizeof(char));
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  int i = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    if (i++ > 10) {
+      WiFi.reconnect();
+      Serial.println(" WiFi reconnect");
+      i = 0;
+    }
   }
 
   Serial.println("");
   Serial.println("WiFi Connecté");
   Serial.println("Adresse IP: ");
   Serial.println(WiFi.localIP());
+
+  display.init();
+  display.flipScreenVertically();
+
 }
 
- bool isNMEAChecksumValid(String sentence){
-   int checksum = 0;
+bool isNMEAChecksumValid(String sentence){
+  int checksum = 0;
 
-   char* tmpLine = tmpNmeaSentence;
-   sentence.toCharArray(tmpLine, sentence.length()+1);
+  char* tmpLine = tmpNmeaSentence;
+  sentence.toCharArray(tmpLine, sentence.length()+1);
 
-   tmpLine++;
-   while (tmpLine[0] != '*') {
-     checksum = checksum ^ tmpLine[0];
-     tmpLine++;
-   }
+  tmpLine++;
+  while (tmpLine[0] != '*') {
+    checksum = checksum ^ tmpLine[0];
+    tmpLine++;
+  }
 
   tmpLine++;
 
@@ -64,7 +85,7 @@ void setup() {
   tmpLine = NULL;
 
   return printedChecksum == checksum;
- }
+}
 
 void connectToNmeaSocket() {
   unsigned long currentMillis = millis();
@@ -83,8 +104,8 @@ void connectToNmeaSocket() {
     url += WiFi.localIP().toString();
 
     client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "NMEA_HOST: " + NMEA_HOST + "\r\n" +
-               "Connection: close\r\n\r\n");
+    "NMEA_HOST: " + NMEA_HOST + "\r\n" +
+    "Connection: close\r\n\r\n");
     unsigned long timeout = millis();
     while (client.available() == 0) {
       if (millis() - timeout > 5000) {
@@ -99,16 +120,17 @@ void connectToNmeaSocket() {
 
 void loop() {
 
-    if(!client.available()){
-      connectToNmeaSocket();
-    } else {
-      NMEA_Line = client.readStringUntil(lf);
-      NMEA_Header = getValue(NMEA_Line, ',', 0).substring(3);
-      if (isNMEAChecksumValid(NMEA_Line)) {
+  if(!client.available()){
+    connectToNmeaSocket();
+  } else {
+    NMEA_Line = client.readStringUntil(lf);
+    NMEA_Header = getValue(NMEA_Line, ',', 0).substring(3);
+    if (isNMEAChecksumValid(NMEA_Line)) {
 
-            if (NMEA_Header == "HDT") {
+      if (NMEA_Header == "HDT") {
         float Cap = getValue(NMEA_Line, ',', 1).toFloat();
         Serial.printf("Cap = %.1f °\n", Cap);
+        display_nmea("Cap = " + String(Cap));
       }
       else if (NMEA_Header == "VTG") {
         float Speed = getValue(NMEA_Line, ',', 7).toFloat();
@@ -117,12 +139,12 @@ void loop() {
       }
       /*TODO à finir MVM
       else if (NMEA_Header == "MVM") {
-      }*/
-      else if (NMEA_Header == "RMC") {
-        String Time = getValue(NMEA_Line, ',', 1);
-        Serial.print("Time = " + Time.substring(0, 2) + ":" + Time.substring(2, 4) + '\n');
-      }
+    }*/
+    else if (NMEA_Header == "RMC") {
+      String Time = getValue(NMEA_Line, ',', 1);
+      Serial.print("Time = " + Time.substring(0, 2) + ":" + Time.substring(2, 4) + '\n');
     }
-
   }
+
+}
 }
